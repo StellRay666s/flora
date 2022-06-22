339f81f1abafce24a8b942a97eda37a81d361b10
Saveliy <ssandry.github@gmail.com>
2022-06-22 14:22:48 +0300

    Add alts attributes

--- src/Components/Cart/Items.jsx
+++ src/Components/Cart/Items.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import '../../index.scss';
-import { Link, Outlet } from 'react-router-dom';
-import axios from 'axios';
+import React from "react";
+import "../../index.scss";
+import { Link, Outlet } from "react-router-dom";
+import axios from "axios";
 function Items() {
   const [items, setTtems] = React.useState([]);
 
   console.log(items);
   React.useEffect(() => {
-    axios.get('https://61efda37732d93001778e635.mockapi.io/flowers/').then((resp) => {
+    axios.get("https://61efda37732d93001778e635.mockapi.io/flowers/").then(resp => {
       const item = resp.data;
       setTtems(item);
     });
   }, []);
   return (
     <>
-      {items.map((obj) => (
+      {items.map(obj => (
         <div className="item" id={obj.id}>
           <div className="image">
             <Link to={`/item/${obj.id}`}>
-              <img src={obj.image} />
+              <img src={obj.image} alt="" />
             </Link>
           </div>
           <h4>{obj.title}</h4>
