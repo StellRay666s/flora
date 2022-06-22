f16ba0ca87f44f89baf9658e52e418ab072f157c
Saveliy <ssandry.github@gmail.com>
2022-06-22 14:39:15 +0300

    Fix imports

--- src/components/index.js
+++ src/components/index.js
@@ -6,4 +6,4 @@ export { default as AboutUs } from "./AboutUs";
 export { default as HowDelevery } from "./HowDelevery";
 export { default as Auth } from "../pages/Auth";
 export { default as Registr } from "../pages/Registr";
-export { default as ThanksPurchase } from "../Components/ThanksPurchase";
+export { default as ThanksPurchase } from "../components/ThanksPurchase";
