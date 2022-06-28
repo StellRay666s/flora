5226eb727ead54b9d70f12385e74d81a6acb5685
Saveliy <ssandry.github@gmail.com>
2022-06-28 20:18:21 +0300

    Not a task. Add imports

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 /** Styles */
-import "./styles/global.scss";
+import "styles/global.scss";
+import "styles/reset.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
