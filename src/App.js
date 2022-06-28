6beb9bc9070a8b9f84b56d67732160fe730e7721
Saveliy <ssandry.github@gmail.com>
2022-06-28 16:42:57 +0300

    Initial project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import React from "react";
+
+/** React Router Dom */
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+/** Pages */
+import IndexPage from "./pages";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<IndexPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
