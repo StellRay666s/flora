3914003e929757341a44b153dede132fc1b962c8
StellRay666s <amazaryan2018@yandex.ru>
2022-06-29 17:23:04 +0300

    'fixed_nameles_icons_imports'

--- src/components/Rating/index.jsx
+++ src/components/Rating/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import StarIcons from "../../icons/StarIcons";
-import StarIconsDisable from "icons/StarIconsDisable";
-
+import StarIcons from "icons/StarIcon";
+import StarIconsDisable from "icons/StarIconDisable";
 import styles from "./starRating.module.scss";
+
 const StarRating = () => {
   const [rating, setRating] = React.useState(0);
   const [hover, setHover] = React.useState(0);
@@ -13,6 +13,7 @@ const StarRating = () => {
         index += 1;
         return (
           <div
+            className={styles.star}
             key={index}
             onMouseEnter={() => setHover(index)}
             onMouseLeave={() => setHover(rating)}
