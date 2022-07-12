cca1c4b0f692f5dee7351d15642c4e64f7a40c8f
saveliy <ssandry.github@gmail.com>
2022-07-12 19:27:23 +0300

    style to styles

--- src/components/PhoneInput/index.jsx
+++ src/components/PhoneInput/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import InputMask from "react-input-mask";
 
-import style from "./index.module.scss";
+import styles from "./index.module.scss";
 
 export default function PhoneInput({ value, dispatchValue }) {
   /**
@@ -14,7 +14,7 @@ export default function PhoneInput({ value, dispatchValue }) {
 
   return (
     <InputMask mask="+7 999 999-99-99" value={value} onChange={onChange}>
-      {inputProps => <input {...inputProps} className={style.inputs} type="tel" />}
+      {inputProps => <input {...inputProps} className={styles.inputs} type="tel" />}
     </InputMask>
   );
 }
