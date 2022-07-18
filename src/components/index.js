298b02f35b70f89967cb087900ec80d9fac4538b
StellRay666s <amazaryan2018@yandex.ru>
2022-07-18 13:03:15 +0300

    'add-radio-component'

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Rating } from "./Rating";
 export { default as CrossClose } from "./CrossClose";
 export { default as Input } from "./Input";
 export { default as PhoneInput } from "./PhoneInput";
+export { default as CheckBox } from "./CheckBox";
