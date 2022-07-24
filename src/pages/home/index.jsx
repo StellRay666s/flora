59844bb2e9e3d18098b35eee7614f0637294a4f8
DanilaAmazaryan <amazaryan2018@yandex.ru>
2022-07-24 20:45:46 +0300

    удалил мусор

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,13 +1,23 @@
 import React, { Fragment, useState } from "react";
 
-import { PhoneInput } from "components";
+import { PhoneInput, CrossClose, Rating, Input, Switch } from "components";
 
 export default function IndexPage() {
   const [name, setName] = useState("");
 
+  console.log(name);
   return (
     <Fragment>
       <PhoneInput value={name} dispatchValue={setName} />
+      <Rating />
+
+      <CrossClose />
+      {/* <Input text={"Имя"} />
+      <Input text={"Адресс"} />
+      <Input text={"Эл.Почта"} />
+      <Input text={"Фамилия"} /> */}
+
+      <Switch />
     </Fragment>
   );
 }
