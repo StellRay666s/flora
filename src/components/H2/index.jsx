004f58e781a35e47d2fb147e7cdabc0c67589678
DanilaAmazaryan <amazaryan2018@yandex.ru>
2022-07-25 00:16:12 +0300

    добавил h2 заголовок

---
+++ src/components/H2/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./index.module.scss";
+
+function H2() {
+  return (
+    <>
+      <h2 className={style.H2}>{value}</h2>
+    </>
+  );
+}
+
+export default H2;
