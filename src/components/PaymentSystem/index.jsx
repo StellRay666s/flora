0b998a809ae54b73472fc1003f6c9699130a3198
saveliy <ssandry.github@gmail.com>
2022-07-30 16:56:38 +0300

    Add images dir

--- src/components/PaymentSystem/index.jsx
+++ src/components/PaymentSystem/index.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import style from "./index.module.scss";
-import paymentLogo from "../../icons/paymentSystem.png";
 
-function PaymentSystem() {
+import PaymentSystemsImage from "assets/images/paymentSystem.png";
+
+function PaymentSystems() {
   return (
-    <>
-      <img
-        alt="Системы платежей"
-        title="Системы платежей"
-        className={style.img}
-        src={paymentLogo}
-      />
-    </>
+    <img
+      alt="Системы платежей"
+      title="Системы платежей"
+      className={style.img}
+      src={PaymentSystemsImage}
+    />
   );
 }
 
-export default PaymentSystem;
+export default PaymentSystems;
