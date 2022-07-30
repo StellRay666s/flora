a0e47eaa20a4b09a05c38113c688aec3d0c85d94
saveliy <ssandry.github@gmail.com>
2022-07-30 17:39:49 +0300

    Fix icons colors

--- src/icons/CreditCartIcon.jsx
+++ src/icons/CreditCartIcon.jsx
@@ -3,6 +3,7 @@ import React from "react";
 export default function CreditCartIcon() {
   return (
     <svg
+      fill="white"
       version="1.1"
       id="Capa_1"
       xmlns="http://www.w3.org/2000/svg"
