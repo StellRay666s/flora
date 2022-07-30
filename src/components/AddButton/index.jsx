64fb6302f07ccf433c20f35a156b4b024a7e849a
saveliy <ssandry.github@gmail.com>
2022-07-30 17:02:47 +0300

    Fix add button

--- src/components/AddButton/index.jsx
+++ src/components/AddButton/index.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import style from "./index.module.scss";
-import { Plus } from "icons";
+import Plus from "icons/Plus";
 
 function AddButton() {
   return (
-    <>
-      <div className={style.svgWrapper}>
-        <Plus />
-      </div>
-    </>
+    <div className={style.svgWrapper}>
+      <Plus />
+    </div>
   );
 }
 
