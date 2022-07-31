233f69d123d99ae1e15792313eb86d48b2598f2a
DanilaAmazaryan <amazaryan2018@yandex.ru>
2022-07-31 13:00:12 +0300

    добавил миник карточку товара

---
+++ src/components/ProductCardMin/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import style from "./index.module.scss";
+
+function ProductCardMin() {
+  return (
+    <>
+      <div className={style.cartWrapper}>
+        <div className={style.cart}></div>
+      </div>
+    </>
+  );
+}
+
+export default ProductCardMin;
