39e28ca4dd78dd4bc8dc35cfa9b59c8a40723415
DanilaAmazaryan <amazaryan2018@yandex.ru>
2022-08-02 00:06:38 +0300

    fixed header

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,5 @@
 import React, { Fragment } from "react";
-import Header from "components/Header";
 
 export default function IndexPage() {
-  return (
-    <Fragment>
-      <Header />
-    </Fragment>
-  );
+  return <Fragment></Fragment>;
 }
