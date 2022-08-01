290501fa8c1cc3ddfca5e51c3ed79460734d19d3
DanilaAmazaryan <amazaryan2018@yandex.ru>
2022-08-01 13:52:55 +0300

    зафиксил засечания

---
+++ src/components/ThanksForOrderModal/index.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import style from "./index.module.scss";
+import { CrossClose, H2, ProductCartMin } from "components";
+
+function ThanksForOrderModal({ isOpen, setIsOpen }) {
+  return (
+    <>
+      {isOpen ? (
+        <div className={style.modalWrapper}>
+          <div className={style.close}>
+            <CrossClose onClick={() => setIsOpen(!isOpen)} />
+          </div>
+          <div className={style.title}>
+            <H2>БЛАГОДАРИМ ЗА ПОКУПКУ</H2>
+          </div>
+          <div className={style.text}>
+            <p>Посмотрите пожалуйста почту, мы отправили туда информацию о закзазе</p>
+          </div>
+          <div className={style.description}>
+            <p>Смотрите так же:</p>
+          </div>
+          <div className={style.miniCart}>
+            <ProductCartMin />
+            <ProductCartMin />
+            <ProductCartMin />
+            <ProductCartMin />
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+    </>
+  );
+}
+
+export default ThanksForOrderModal;
