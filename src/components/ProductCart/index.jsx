f9838a6b15913be1944bfb958492772622ae2224
DanilaAmazaryan <amazaryan2018@yandex.ru>
2022-08-01 23:29:55 +0300

    компонент карточки товара, название в 2 строки сделать не получилось

---
+++ src/components/ProductCart/index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import style from "./index.module.scss";
+import AddButton from "components/AddButton";
+
+function ProductCart() {
+  return (
+    <>
+      <div className={style.wrapper}>
+        <div className={style.cartWrapper}>
+          <div className={style.imgBorder}>
+            <div className={style.border}>
+              <img className={style.img} />
+            </div>
+          </div>
+          <div className={style.title}>Цветочная композиция роза и гербена</div>
+          <span className={style.price}>4 440 руб.</span>
+          <div className={style.addBusket}>
+            <AddButton />
+            <p>Добавить в корзину</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ProductCart;
