5180712c32714c86bf2c94a6bd89b1e17e516d60
DanilaAmazaryan <amazaryan2018@yandex.ru>
2022-08-04 23:09:43 +0300

    fix conflicts

--- src/icons/BoqueteIcon.jsx
+++ src/icons/BoqueteIcon.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function BoqueteIcon({ color, height, width }) {
   return (
     <svg
-      fill="white"
+      fill="black"
       version="1.1"
       id="Layer_1"
       xmlns="http://www.w3.org/2000/svg"
