108f7b79fb0c611200319bb8d00afb211b799382
DanilaAmazaryan <amazaryan2018@yandex.ru>
2022-08-05 22:32:43 +0300

    решал конфликты

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,5 @@
 import React, { Fragment } from "react";
-import OrderGuideComponent from "components/OrderGuideComponent";
 
 export default function IndexPage() {
-  return (
-    <Fragment>
-      <OrderGuideComponent />
-    </Fragment>
-  );
+  return <Fragment></Fragment>;
 }
