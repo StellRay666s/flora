fab19de0c0154c27fc7761db841a37a3f58277ff
DanilaAmazaryan <amazaryan2018@yandex.ru>
2022-08-07 13:56:25 +0300

    добавил перехватчик

---
+++ src/utils/initializeInterceptor.js
@@ -0,0 +1,8 @@
+import { axiosClient } from "./axiosClient";
+
+function initializeInterceptor() {
+  axiosClient.interceptors.request.use(config => {
+    config.headers.Authorization = window.localStorage.getItem("token");
+    return config;
+  });
+}
