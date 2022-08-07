376b6248117b89e5b358bf5cf091ded6bf3d78f0
DanilaAmazaryan <amazaryan2018@yandex.ru>
2022-08-07 18:29:37 +0300

    бля сорри очень тяжко мне пока что с серваком общаться

--- src/utils/initializeInterceptor.js
+++ src/utils/initializeInterceptor.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { axiosClient } from "./axiosClient";
 
 function initializeInterceptor() {
@@ -6,4 +7,66 @@ function initializeInterceptor() {
     return config;
   });
 }
+
+function postRefreshToken() {
+  axiosClient.interceptors.use(
+    async function (respons) {
+      return respons;
+    },
+    async function (error) {
+      if (error.respons.status !== 401) {
+        return new Promise((resolve, reject) => {
+          reject(error);
+        });
+      }
+      const config = error.config;
+
+      const refreshToken = localStorage.getItem("STORAGE_REFRESH_TOKEN");
+
+      if (!refreshToken) {
+        return Promise.reject(error);
+      }
+
+      let res;
+      try {
+        res = await axios.post(`${process.env.REACT_APP_API_KEY}/authentication/refresh`, {
+          refreshToken,
+        });
+      } catch (err) {
+        console.log(err);
+        localStorage.removeItem("STORAGE_REFRESH_TOKEN");
+        localStorage.removeItem("STORAGE_ACCESS_TOKEN");
+      }
+      config.headers.Authorization = `Bearer ${res.data.accessToken}`;
+
+      localStorage.setItem("STORAGE_REFRESH_TOKEN", res.data.refreshToken);
+      localStorage.setItem("STORAGE_ACCESS_TOKEN", res.data.accessToken);
+
+      return new Promise((resolve, reject) => {
+        axios
+          .request(config)
+          .then(response => {
+            resolve(response);
+          })
+          .catch(error => {
+            reject(error);
+          });
+      });
+    }
+  );
+
+  axiosClient.interceptors.request.use(
+    async function (config) {
+      /** если токен есть в стораге то мы его суем в запрос */
+      if (localStorage.getItem("STORAGE_ACCESS_TOKEN")) {
+        config.headers.Authorization = `Bearer ${localStorage.getItem("STORAGE_ACCESS_TOKEN")}`;
+      }
+      return config;
+    },
+    async function (error) {
+      return Promise.reject(error);
+    }
+  );
+}
+
 export { initializeInterceptor };
