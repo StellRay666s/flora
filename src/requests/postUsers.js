95db1fc9bcc3ea0f67bb5a6dd6baf814868d3d54
DanilaAmazaryan <amazaryan2018@yandex.ru>
2022-08-09 12:28:14 +0300

    add post users request

---
+++ src/requests/postUsers.js
@@ -0,0 +1,14 @@
+import { axiosClient } from "utils/axiosClient";
+
+async function postUsers(email, password, address, phone, name) {
+  const response = await axiosClient.post("/users", {
+    email: email,
+    password: password,
+    address: address,
+    phone: phone,
+    name: name,
+  });
+  return response;
+}
+
+export { postUsers };
