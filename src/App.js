81f070e548b65ebf68cd549865d1e8e335e3f98d
DanilaAmazaryan <amazaryan2018@yandex.ru>
2022-08-09 13:32:55 +0300

    удалял лишнее

--- src/App.js
+++ src/App.js
@@ -6,10 +6,8 @@ import { store } from "redux/store";
 
 /** Routing */
 import Router from "router";
-import { postAutentication } from "requests/postAutentication";
 
 function App() {
-  postAutentication();
   return (
     <Provider store={store}>
       <Fragment>
