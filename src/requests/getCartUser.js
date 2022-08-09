3f41c51a73f66ee52bb2a1240c0ebd174092afdb
DanilaAmazaryan <amazaryan2018@yandex.ru>
2022-08-09 18:52:58 +0300

    fix e

--- src/requests/getCartUser.js
+++ src/requests/getCartUser.js
@@ -1,8 +1,8 @@
 import { axiosClient } from "utils/axiosClient";
 
 async function getCartUsers() {
-  const respons = await axiosClient.get("/cart");
-  return respons;
+  const response = await axiosClient.get("/cart");
+  return response;
 }
 
 export { getCartUsers };
