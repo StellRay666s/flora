9d69acc8059f38c72e993b72bd7bd4dd679c8636
DanilaAmazaryan <amazaryan2018@yandex.ru>
2022-08-09 18:48:37 +0300

    add fixes2

--- src/requests/postReAuth.js
+++ src/requests/postReAuth.js
@@ -3,6 +3,7 @@ import { axiosClient } from "utils/axiosClient";
 async function postReAuth(accessToken) {
   const response = await axiosClient.post("/authentication", {
     accessToken: accessToken,
+    strategy: "jwt",
   });
   return response;
 }
