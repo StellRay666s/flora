d907a99d33cba18d48e342806095a709dfe982a0
DanilaAmazaryan <amazaryan2018@yandex.ru>
2022-08-12 15:15:39 +0300

    fixes

--- src/requests/postUsers.js
+++ src/requests/postUsers.js
@@ -1,6 +1,6 @@
 import { axiosClient } from "utils/axiosClient";
 
-async function postUsers(email, password, address, phone, name) {
+async function postUsers(email = "", password = "", address = "", phone = "", name = "") {
   const response = await axiosClient.post("/users", {
     email: email,
     password: password,
