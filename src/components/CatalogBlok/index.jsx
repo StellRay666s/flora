2d305aa2067854b7539a3126df9b55db394db875
DanilaAmazaryan <amazaryan2018@yandex.ru>
2022-08-18 08:56:30 +0300

    add-loading-indikator

--- src/components/CatalogBlok/index.jsx
+++ src/components/CatalogBlok/index.jsx
@@ -2,18 +2,25 @@ import React from "react";
 import style from "./index.module.scss";
 import ProductCard from "components/ProductCard";
 import H1 from "components/H1";
+import Circle2 from "assets/gif/Circle2.gif";
 
-function CatalogBlock({ data }) {
+function CatalogBlock({ data, isLoading }) {
   return (
     <>
       {" "}
-      <h1>
+      <h1 className={style.title}>
         <H1>КАТАЛОГ</H1>
       </h1>
       <div className={style.wrapperCarts}>
-        {data.map(obj => (
-          <ProductCard {...obj} />
-        ))}
+        {data.map(obj =>
+          isLoading ? (
+            <ProductCard {...obj} />
+          ) : (
+            <div className={style.Loading}>
+              <img alt="...Загрузка" src={Circle2} />
+            </div>
+          )
+        )}
       </div>
     </>
   );
