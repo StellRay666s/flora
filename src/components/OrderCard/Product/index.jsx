13ff21efc6eee4e3656b2f809af48a310ed3d8f1
DanilaAmazaryan <amazaryan2018@yandex.ru>
2022-08-28 13:42:24 +0300

    fix layaout oreder

--- src/components/OrderCard/Product/index.jsx
+++ src/components/OrderCard/Product/index.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import style from "./index.module.scss";
 
-function Product({ title = "название", image }) {
+function Product({ title = "название", img, alt = "Изображение цветов" }) {
   return (
     <div className={style.item}>
       <div className={style.image}>
-        <img src="#" />
+        <img src={img} alt={alt} />
       </div>
-      <div className={style.title}>Ромашки</div>
+      <div className={style.title}>{title}</div>
     </div>
   );
 }
