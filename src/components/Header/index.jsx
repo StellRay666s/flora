82c9d2d7ebd0ac018206d6f9af9233b0f234c23c
DanilaAmazaryan <amazaryan2018@yandex.ru>
2022-09-03 00:27:41 +0300

    add auth fix

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,33 +1,48 @@
 import React from "react";
-import { Link } from "react-scroll";
+import { Link } from "react-router-dom";
+import { Link as Scroll } from "react-scroll";
 import Logo from "components/Logo";
 import CartButton from "components/CartButton";
+import { useSelector } from "react-redux";
 
 import style from "./index.module.scss";
 
 function Header() {
+  const { isAuth } = useSelector(store => store.user.user);
+
   return (
     <div className={style.header}>
       <div className={style.logo}>
         <Logo />
       </div>
       <ul className={style.barList}>
-        <Link>
+        <Scroll>
           <li>Главная</li>
-        </Link>
-        <Link>
+        </Scroll>
+        <Scroll>
           <li>Каталог</li>
-        </Link>
-        <Link>
+        </Scroll>
+        <Scroll>
           <li>О нас</li>
-        </Link>
-        <Link>
+        </Scroll>
+        <Scroll>
           <li>Как сделать заказы</li>
-        </Link>
+        </Scroll>
 
-        <Link>
-          <li>Заказы</li>
-        </Link>
+        {isAuth ? (
+          <Link to={"order/:_id"}>
+            <li>Заказы</li>
+          </Link>
+        ) : (
+          <div className={style.auth}>
+            <Link to={"/authorization"}>
+              <li>Авторизация</li>
+            </Link>
+            <Link to={"/registration"}>
+              <li>Регистрация</li>
+            </Link>
+          </div>
+        )}
       </ul>
       <CartButton />
     </div>
